c9d6ffd0d97546f27c408ba6e4ab5d9370f6b207
Harry Manley <niavasha@gmail.com>
2020-04-14 20:07:57 -0700

    debugging

--- tvhProxy.py
+++ tvhProxy.py
@@ -1,18 +1,19 @@
-from gevent import monkey; monkey.patch_all()
-
-import sched
-import time
-import os
-import requests
-import threading
-import socket
-import logging
-from datetime import timedelta, datetime, time
-import xml.etree.ElementTree as ElementTree
-from gevent.pywsgi import WSGIServer
-from flask import Flask, Response, request, jsonify, abort, render_template
-from ssdp import SSDPServer
 from dotenv import load_dotenv
+from ssdp import SSDPServer
+from flask import Flask, Response, request, jsonify, abort, render_template
+from gevent.pywsgi import WSGIServer
+import xml.etree.ElementTree as ElementTree
+from datetime import timedelta, datetime, time
+import logging
+import socket
+import threading
+import requests
+import os
+import time
+import sched
+from gevent import monkey
+monkey.patch_all()
+
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv(verbose=True)
@@ -22,20 +23,24 @@ scheduler = sched.scheduler()
 logger = logging.getLogger()
 
 host_name = socket.gethostname()
-host_ip = socket.gethostbyname(host_name) 
+host_ip = socket.gethostbyname(host_name)
 
 # URL format: <protocol>://<username>:<password>@<hostname>:<port>, example: https://test:1234@localhost:9981
 config = {
     'deviceID': os.environ.get('DEVICE_ID') or '12345678',
     'bindAddr': os.environ.get('TVH_BINDADDR') or '',
     'tvhURL': os.environ.get('TVH_URL') or 'http://test:test@localhost:9981',
-    'tvhProxyURL': os.environ.get('TVH_PROXY_URL'), # only used if set (in case of forward-proxy), otherwise assembled from host + port bel
+    # only used if set (in case of forward-proxy), otherwise assembled from host + port bel
+    'tvhProxyURL': os.environ.get('TVH_PROXY_URL'),
     'tvhProxyHost': os.environ.get('TVH_PROXY_HOST') or host_ip,
     'tvhProxyPort': os.environ.get('TVH_PROXY_PORT') or 5004,
-    'tunerCount': os.environ.get('TVH_TUNER_COUNT') or 6,  # number of tuners in tvh
+    # number of tuners in tvh
+    'tunerCount': os.environ.get('TVH_TUNER_COUNT') or 6,
     'tvhWeight': os.environ.get('TVH_WEIGHT') or 300,  # subscription priority
-    'chunkSize': os.environ.get('TVH_CHUNK_SIZE') or 1024*1024,  # usually you don't need to edit this
-    'streamProfile': os.environ.get('TVH_PROFILE') or 'pass'  # specifiy a stream profile that you want to use for adhoc transcoding in tvh, e.g. mp4
+    # usually you don't need to edit this
+    'chunkSize': os.environ.get('TVH_CHUNK_SIZE') or 1024*1024,
+    # specifiy a stream profile that you want to use for adhoc transcoding in tvh, e.g. mp4
+    'streamProfile': os.environ.get('TVH_PROFILE') or 'pass'
 }
 
 discoverData = {
@@ -45,11 +50,12 @@ discoverData = {
     'FirmwareName': 'hdhomeruntc_atsc',
     'TunerCount': int(config['tunerCount']),
     'FirmwareVersion': '20150826',
-    'DeviceID': config['deviceID'], 
+    'DeviceID': config['deviceID'],
     'DeviceAuth': 'test1234',
     'BaseURL': '%s' % (config['tvhProxyURL'] or "http://" + config['tvhProxyHost'] + ":" + str(config['tvhProxyPort'])),
     'LineupURL': '%s/lineup.json' % (config['tvhProxyURL'] or "http://" + config['tvhProxyHost'] + ":" + str(config['tvhProxyPort']))
 }
+
 
 @app.route('/discover.json')
 def discover():
@@ -72,7 +78,8 @@ def lineup():
 
     for c in _get_channels():
         if c['enabled']:
-            url = '%s/stream/channel/%s?profile=%s&weight=%s' % (config['tvhURL'], c['uuid'], config['streamProfile'],int(config['tvhWeight']))
+            url = '%s/stream/channel/%s?profile=%s&weight=%s' % (
+                config['tvhURL'], c['uuid'], config['streamProfile'], int(config['tvhWeight']))
 
             lineup.append({'GuideNumber': str(c['number']),
                            'GuideName': c['name'],
@@ -100,7 +107,7 @@ def epg():
 
 def _get_channels():
     url = '%s/api/channel/grid?start=0&limit=999999' % config['tvhURL']
-
+    logger.info('downloading channels from %s', url)
     try:
         r = requests.get(url)
         return r.json()['entries']
@@ -144,15 +151,19 @@ def _get_xmltv():
                              key, channelName)
 
                 # create 2h programmes for 72 hours
-                yesterday_midnight = datetime.combine(datetime.today(), time.min) - timedelta(days=1)
+                yesterday_midnight = datetime.combine(
+                    datetime.today(), time.min) - timedelta(days=1)
                 date_format = '%Y%m%d%H%M%S'
 
                 for x in range(0, 36):
                     dummyProgramme = ElementTree.SubElement(root, 'programme')
                     dummyProgramme.attrib['channel'] = str(key)
-                    dummyProgramme.attrib['start'] = (yesterday_midnight + timedelta(hours=x*2)).strftime(date_format)
-                    dummyProgramme.attrib['stop'] = (yesterday_midnight + timedelta(hours=(x*2)+2)).strftime(date_format)
-                    dummyTitle = ElementTree.SubElement(dummyProgramme, 'title')
+                    dummyProgramme.attrib['start'] = (
+                        yesterday_midnight + timedelta(hours=x*2)).strftime(date_format)
+                    dummyProgramme.attrib['stop'] = (
+                        yesterday_midnight + timedelta(hours=(x*2)+2)).strftime(date_format)
+                    dummyTitle = ElementTree.SubElement(
+                        dummyProgramme, 'title')
                     dummyTitle.attrib['lang'] = 'eng'
                     dummyTitle.text = channelName
                     dummyDesc = ElementTree.SubElement(dummyProgramme, 'desc')
@@ -163,7 +174,7 @@ def _get_xmltv():
         return ElementTree.tostring(root)
     except requests.exceptions.RequestException as e:  # This is the correct syntax
         logger.error('An error occured: %s' + repr(e))
-    
+
 
 def _start_ssdp():
     ssdp = SSDPServer()
